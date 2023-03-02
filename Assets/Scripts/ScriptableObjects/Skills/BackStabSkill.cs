01496d56bb41d183e60d3f576e1a9fd964a5cd9d
alangamu <alangamu@gmail.com>
2023-03-02 10:55:42 -0300

    first skill

---
+++ Assets/Scripts/ScriptableObjects/Skills/BackStabSkill.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoFantasy.Scripts.ScriptableObjects.Skills
+{
+    public class BackStabSkill : SkillSO
+    {
+        public override void PerformSkill()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
