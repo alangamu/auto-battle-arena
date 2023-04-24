04417a227ee4fc39a50059feb45f9c308f02f30f
alangamu <alangamu@gmail.com>
2023-04-24 15:39:10 -0400

    click sound of buttons

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using AutoFantasy.Scripts.ScriptableObjects;
+using UnityEngine;
+
+namespace AutoFantasy.Scripts
+{
+    public class AudioManager : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioEvent _audioEvent;
+        [SerializeField]
+        private AudioSource _audioSource;
+
+        private void OnEnable()
+        {
+            _audioEvent.OnRaise += PlayAudio;
+        }
+
+        private void OnDisable()
+        {
+            _audioEvent.OnRaise -= PlayAudio;
+        }
+
+        private void PlayAudio(AudioClip clip)
+        {
+            _audioSource.clip = clip;
+            _audioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/Events/AudioEvent.cs
@@ -0,0 +1,11 @@
+﻿using AutoFantasy.Scripts.ScriptableObjects.Events;
+using UnityEngine;
+
+namespace AutoFantasy.Scripts.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "Game Events/Sound Event")]
+    public class AudioEvent : BaseGameEvent<AudioClip>
+    {
+
+    }
+}
